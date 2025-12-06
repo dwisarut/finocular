125007240dbf0ec80b468cccdd75e63dbbaba304
d_wisarut <d.wisarut@hotmail.com>
2025-12-07 03:21:43 +0700

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard.tsx";
+import LandingPage from "./components/LandingPage.tsx";
+import Ledger from "./components/Ledger.tsx";
+import SignUp from "./components/SignUp.tsx";
+import LogIn from "./components/LogIn.tsx";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <div className="overflow-x-hidden">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/ledger" element={<Ledger />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<LogIn />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
