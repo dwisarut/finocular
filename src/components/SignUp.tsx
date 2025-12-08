a99deef59cbb5ecffe10ba6076f82141b49ae562
d_wisarut <d.wisarut@hotmail.com>
2025-12-08 14:39:38 +0700

    feat: add signin and login components

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,13 @@
 const SignUp = () => {
   return (
     <>
-      <div></div>
+      <div className="flex container justify-center mt-24">
+        <div className="flex flex-col border border-border rounded-2xl p-8 w-4xl items-center">
+          <h1 className="flex text-contrast-text text-2xl">
+            Sign in to your account
+          </h1>
+        </div>
+      </div>
     </>
   );
 };
