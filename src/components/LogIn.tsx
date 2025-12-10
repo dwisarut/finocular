adbaf1a8e408f814a53b7e7a03807e288c4c58ab
d_wisarut <d.wisarut@hotmail.com>
2025-12-11 02:54:48 +0700

    feat: add responsive layout within border

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -7,7 +7,7 @@ const LogIn = () => {
   return (
     <>
       <div className="flex flex-col container justify-center mt-24">
-        <div className="flex flex-col border border-border rounded-2xl p-8 w-xl items-center self-center">
+        <div className="flex flex-col border border-border rounded-2xl p-8 w-100 md:w-xl items-center self-center">
           <h1 className="flex text-contrast-text text-2xl">
             Log in to your account
           </h1>
