0723ca41df308583f877528d0bf080c725c47eb7
d_wisarut <d.wisarut@hotmail.com>
2025-12-10 14:37:44 +0700

    feat: add remember me checkbox

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,39 +1,46 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import Checkbox from "@mui/material/Checkbox";
 
 const LogIn = () => {
   const { register, handleSubmit } = useForm();
   return (
     <>
-      <div className="flex container justify-center mt-24">
-        <div className="flex flex-col border border-border rounded-2xl p-8 w-4xl items-center">
+      <div className="flex flex-col container justify-center mt-24">
+        <div className="flex flex-col border border-border rounded-2xl p-8 w-xl items-center self-center">
           <h1 className="flex text-contrast-text text-2xl">
             Log in to your account
           </h1>
           <form
-            className="flex flex-col text-contrast-text w-full m-4 mt-8 gap-8"
+            className="flex flex-col text-contrast-text w-3/4 m-4 mt-8 gap-8"
             onSubmit={handleSubmit((data) => {
               console.log("Receive the data!", data);
             })}
           >
             <input
-              {...register("username", { required: true, minLength: 8 })}
-              placeholder="Username or email"
+              type="email"
+              {...register("email-address", { required: true })}
+              placeholder="Email"
               className="h-8 border-b outline-0"
             />
             <input
+              type="password"
               {...register("password", { required: true, minLength: 8 })}
               placeholder="Password"
               className="h-8 border-b outline-0"
             />
+            <div className="flex flex-row items-center -ml-3">
+              <Checkbox className="w-fit" defaultChecked />
+              <p>Remember me on this device</p>
+            </div>
             <input
               type="submit"
               className="self-center border border-border rounded-2xl h-8 w-3xs hover:cursor-pointer"
             />
-            <p className="text-contrast-text self-center lato">
+            <p className="text-contrast-text self-center lato text-sm">
               New to Finocular?{" "}
               <Link to="/signup">
-                <span className="text-contrast-text hover:text-amber-300">
+                <span className="text-contrast-text hover:text-amber-300 text-sm">
                   Create account
                 </span>
               </Link>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import Checkbox from "@mui/material/Checkbox";
 
 const SignUp = () => {
   const { register, handleSubmit } = useForm();
@@ -8,7 +9,7 @@ const SignUp = () => {
       <div className="flex flex-col container justify-center mt-24">
         <div className="flex flex-col border border-border rounded-2xl p-8 w-xl items-center self-center">
           <h1 className="flex text-contrast-text text-2xl">
-            Sign up your account
+            Sign up to your account
           </h1>
           <form
             className="flex flex-col text-contrast-text w-3/4 m-4 mt-8 gap-8"
@@ -42,6 +43,10 @@ const SignUp = () => {
               placeholder="Confirm password"
               className="h-8 border-b outline-0"
             />
+            <div className="flex flex-row items-center -ml-3">
+              <Checkbox className="w-fit" defaultChecked />
+              <p>Remember me on this device</p>
+            </div>
             <input
               type="submit"
               className="self-center border border-border rounded-2xl h-8 w-3xs hover:cursor-pointer"
