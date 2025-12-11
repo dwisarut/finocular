f148d902a29f4a966d21947797132a0925bc1321
d_wisarut <d.wisarut@hotmail.com>
2025-12-11 21:24:09 +0700

    feat: add shadcn-ui components

--- src/components/LedgerTable.tsx
+++ src/components/LedgerTable.tsx
@@ -1,11 +1,3 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-
 // PLACEHOLDER
 
 // interface TransactionData {
@@ -32,37 +24,7 @@ const rows = [
 ];
 
 const LedgerTable = () => {
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Transaction type</TableCell>
-              <TableCell align="right">Value&nbsp;(THB)</TableCell>
-              <TableCell align="right">Sender</TableCell>
-              <TableCell align="right">Recipient</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.type}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.type}
-                </TableCell>
-                <TableCell align="right">{row.value}</TableCell>
-                <TableCell align="right">{row.sender}</TableCell>
-                <TableCell align="right">{row.recipient}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  );
+  return <></>;
 };
 
 export default LedgerTable;
