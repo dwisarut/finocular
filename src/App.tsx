e284e2f141ded857b05c5bf8312635c0f14ded96
d_wisarut <d.wisarut@hotmail.com>
2025-12-11 15:44:06 +0700

    feat: add shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,21 @@ import Ledger from "./components/Ledger.tsx";
 import SignUp from "./components/SignUp.tsx";
 import LogIn from "./components/LogIn.tsx";
 import Navbar from "./components/Navbar.tsx";
-import { Provider } from "./components/ui/provider.tsx";
 
 function App() {
   return (
-    <Provider>
-      <BrowserRouter>
-        <div className="overflow-x-hidden">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/ledger" element={<Ledger />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<LogIn />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <div className="overflow-x-hidden">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/ledger" element={<Ledger />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
