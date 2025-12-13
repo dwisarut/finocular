f57442f237ab047ecb4c9e6b4dddf4a5458ff304
d_wisarut <d.wisarut@hotmail.com>
2025-12-13 16:09:01 +0700

    feat: add shadcn table with a mock data

--- src/components/LedgerTable.tsx
+++ src/components/LedgerTable.tsx
@@ -1,11 +1,13 @@
-// PLACEHOLDER
-
-// interface TransactionData {
-//   type: "Revenue" | "Expense";
-//   value: number;
-//   sender: string;
-//   recipient: string;
-// }
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const FetchData = (
   type: "Revenue" | "Expense",
@@ -24,7 +26,44 @@ const rows = [
 ];
 
 const LedgerTable = () => {
-  return <></>;
+  const summation = () => {
+    let sum = 0;
+    for (let i = 0; i < rows.length; i++) {
+      sum += rows[i].value;
+    }
+    return sum;
+  };
+  return (
+    <>
+      <Table>
+        <TableCaption>A list of your recent transactions.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Type</TableHead>
+            <TableHead>Sender</TableHead>
+            <TableHead>Recipient</TableHead>
+            <TableHead className="text-right">Amount (THB)</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rows.map((rec) => (
+            <TableRow key={rec.type}>
+              <TableCell className="font-medium">{rec.type}</TableCell>
+              <TableCell>{rec.sender}</TableCell>
+              <TableCell>{rec.recipient}</TableCell>
+              <TableCell className="text-right">{rec.value}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">{summation()}</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </>
+  );
 };
 
 export default LedgerTable;
