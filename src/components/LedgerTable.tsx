38dd1925c5022fdff0a96a629e43a861f6ae29da
d_wisarut <d.wisarut@hotmail.com>
2025-12-19 17:47:23 +0700

    feat: add empty state table cell

--- src/components/LedgerTable.tsx
+++ src/components/LedgerTable.tsx
@@ -27,27 +27,34 @@ function LedgerTable() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {rows.map((rec) => {
-            const displayDate = new Date(rec.timestamp).toLocaleDateString(
-              "en-GB",
-              {
-                day: "2-digit",
-                month: "short",
-                year: "numeric",
-              }
-            );
-
-            return (
-              <TableRow key={rec.ref}>
-                <TableCell className="font-medium lato">{rec.type}</TableCell>
-                <TableCell className="lato">{displayDate}</TableCell>
-                <TableCell className="lato">{rec.sender}</TableCell>
-                <TableCell className="lato">{rec.recipient}</TableCell>
-                <TableCell className="lato">{rec.desc}</TableCell>
-                <TableCell className="text-right lato">{rec.value}</TableCell>
-              </TableRow>
-            );
-          })}
+          {rows.length === 0 ? (
+            <TableRow>
+              <TableCell className="font-medium lato text-center" colSpan={6}>
+                Add new transaction
+              </TableCell>
+            </TableRow>
+          ) : (
+            rows.map((rec) => {
+              const displayDate = new Date(rec.timestamp).toLocaleDateString(
+                "en-GB",
+                {
+                  day: "2-digit",
+                  month: "short",
+                  year: "numeric",
+                }
+              );
+              return (
+                <TableRow key={rec.ref}>
+                  <TableCell className="font-medium lato">{rec.type}</TableCell>
+                  <TableCell className="lato">{displayDate}</TableCell>
+                  <TableCell className="lato">{rec.sender}</TableCell>
+                  <TableCell className="lato">{rec.recipient}</TableCell>
+                  <TableCell className="lato">{rec.desc}</TableCell>
+                  <TableCell className="text-right lato">{rec.value}</TableCell>
+                </TableRow>
+              );
+            })
+          )}
         </TableBody>
         <TableFooter>
           <TableRow>
