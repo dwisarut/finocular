9e7e69b5512078c89b6b4742d41a85b767f9c1cb
d_wisarut <d.wisarut@hotmail.com>
2025-12-30 21:45:08 +0700

    feat: add working add data button

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -20,14 +20,64 @@ import {
 } from "./ui/select";
 import { Label } from "./ui/label";
 import { Plus, ChevronDownIcon } from "lucide-react";
-import { useState } from "react";
+import { useState, type MouseEvent } from "react";
 import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
 import { Calendar } from "./ui/calendar";
 import { Input } from "./ui/input";
 
 function AddButton() {
   const [open, setOpen] = useState(false);
+  const [type, setType] = useState<"revenue" | "expense" | "">("");
   const [date, setDate] = useState<Date | undefined>(undefined);
+  const [sender, setSender] = useState("");
+  const [recipient, setRecipient] = useState("");
+  const [category, setCategory] = useState<
+    | "income"
+    | "saving_investment"
+    | "shopping"
+    | "entertainment"
+    | "billing"
+    | "drinking_food"
+    | "vacation"
+    | "others"
+    | ""
+  >("");
+  const [amount, setAmount] = useState("");
+
+  const onSubmitForm = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      const body = {
+        type,
+        date: date ? date.toISOString() : null,
+        sender,
+        recipient,
+        category,
+        amount,
+      };
+      const response = await fetch("http://localhost:3000/api/transactions", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      if (response.ok) {
+        setType("");
+        setDate(undefined);
+        setSender("");
+        setRecipient("");
+        setCategory("");
+        setAmount("");
+
+        console.log("Transaction added successfully!");
+      } else {
+        console.error("Failed to add transaction");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
@@ -61,7 +111,14 @@ function AddButton() {
               <div className="flex flex-row justify-between">
                 <div className="flex flex-col gap-3">
                   <Label htmlFor="type">Type</Label>
-                  <Select>
+                  <Select
+                    onValueChange={(type) => {
+                      if (type === "revenue" || type === "expense") {
+                        setType(type);
+                      }
+                    }}
+                    value={type}
+                  >
                     <SelectTrigger
                       id="type"
                       className="w-[140px] rounded-[0.5rem] hover:cursor-pointer"
@@ -71,8 +128,8 @@ function AddButton() {
                     <SelectContent className="rounded-[0.5rem] w-[140px]">
                       <SelectGroup>
                         <SelectLabel>Types</SelectLabel>
-                        <SelectItem value="Revenue">Revenue</SelectItem>
-                        <SelectItem value="Expense">Expense</SelectItem>
+                        <SelectItem value="revenue">Revenue</SelectItem>
+                        <SelectItem value="expense">Expense</SelectItem>
                       </SelectGroup>
                     </SelectContent>
                   </Select>
@@ -112,16 +169,38 @@ function AddButton() {
               <Input
                 type="text"
                 placeholder="Sender name"
+                name="sender_name"
                 className="rounded-[0.5rem]"
+                value={sender}
+                onChange={(e) => setSender(e.target.value)}
               />
               <Input
                 type="text"
                 placeholder="Recipient name"
+                name="recipient_name"
                 className="rounded-[0.5rem]"
+                value={recipient}
+                onChange={(e) => setRecipient(e.target.value)}
               />
               <div className="flex flex-col gap-3">
                 <Label htmlFor="category">Category</Label>
-                <Select>
+                <Select
+                  onValueChange={(category) => {
+                    if (
+                      category === "income" ||
+                      category === "saving_investment" ||
+                      category === "shopping" ||
+                      category === "entertainment" ||
+                      category === "billing" ||
+                      category === "drinking_food" ||
+                      category === "vacation" ||
+                      category === "others"
+                    ) {
+                      setCategory(category);
+                    }
+                  }}
+                  value={category}
+                >
                   <SelectTrigger
                     id="category"
                     className="w-full rounded-[0.5rem] hover:cursor-pointer"
@@ -152,7 +231,12 @@ function AddButton() {
               <Input
                 type="number"
                 placeholder="Amount in THB"
+                name="money_amount"
                 className="rounded-[0.5rem]"
+                value={amount}
+                onChange={(e) => {
+                  setAmount(e.target.value);
+                }}
               />
             </div>
             <DialogFooter>
@@ -167,6 +251,9 @@ function AddButton() {
               <Button
                 type="submit"
                 className="rounded-[0.5rem] hover:cursor-pointer"
+                onClick={(e) => {
+                  onSubmitForm(e);
+                }}
               >
                 Save changes
               </Button>
