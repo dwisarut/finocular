7c53db9f8828b33330bc37d059bc09d86849f8c4
d_wisarut <d.wisarut@hotmail.com>
2026-01-07 16:35:16 +0700

    feat: add functional render summary

--- src/components/RecentTransaction.tsx
+++ src/components/RecentTransaction.tsx
@@ -1,10 +1,47 @@
-// NOTE: This is the recent transaction history
-// COMPONENTS: Cards
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import rows from "./MockData";
+import { useEffect, useState } from "react";
+
+type Transaction = {
+  id: number;
+  type: "revenue" | "expense";
+  date: string; // ISO string from API
+  sender: string;
+  recipient: string;
+  category:
+    | "income"
+    | "saving_investment"
+    | "shopping"
+    | "entertainment"
+    | "billing"
+    | "drinking_food"
+    | "vacation"
+    | "others";
+  amount: string;
+};
 
 function TransactionSection() {
+  const [recentTransaction, setRecentTransaction] = useState<
+    Transaction[] | null
+  >(null);
+
+  useEffect(() => {
+    const fetchRecentTransaction = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/api/transactions/summary/recent-transaction"
+        );
+
+        const data: Transaction[] = await response.json();
+        setRecentTransaction(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchRecentTransaction();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col gap-6">
@@ -13,36 +50,31 @@ function TransactionSection() {
           <CardContent>
             <ScrollArea className="w-full whitespace-nowrap">
               <div className="flex flex-row gap-4 p-4">
-                {rows.map((data) => {
-                  const isRevenue = data.type === "revenue";
+                {recentTransaction && recentTransaction.length > 0 ? (
+                  recentTransaction.map((data) => {
+                    const isRevenue = data.type === "revenue";
 
-                  if (isRevenue)
                     return (
-                      <Card key={data.ref} className="min-w-[200px]">
+                      <Card key={data.id} className="min-w-[200px]">
                         <CardHeader>
                           <CardTitle className="Alata">{data.type}</CardTitle>
                         </CardHeader>
                         <CardContent>
-                          <p className="lato text-green-400">
-                            + {data.value} THB
+                          <p
+                            className={`lato ${
+                              isRevenue ? " text-green-400" : "text-red-400"
+                            }`}
+                          >
+                            {isRevenue ? "+" : "-"}
+                            {data.amount} THB
                           </p>
                         </CardContent>
                       </Card>
                     );
-                  else
-                    return (
-                      <Card key={data.ref} className="min-w-[200px]">
-                        <CardHeader>
-                          <CardTitle className="Alata">{data.type}</CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                          <p className="lato text-red-400">
-                            - {data.value} THB
-                          </p>
-                        </CardContent>
-                      </Card>
-                    );
-                })}
+                  })
+                ) : (
+                  <h1>No recent transaction</h1>
+                )}
               </div>
               <ScrollBar orientation="horizontal" className="mt-6" />
             </ScrollArea>
--- src/components/SummaryDB.tsx
+++ src/components/SummaryDB.tsx
@@ -1,27 +1,33 @@
-// NOTE: This is the summary component in dashboard
-// COMPONENTS: Avatar, total earn or spend, net loss, net gain, cash flow visualization
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import summation from "./Summation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import revenue from "./MockRevenue";
-import expense from "./MockExpense";
+import { useEffect, useState } from "react";
 
 function SummarySection() {
-  const revSum = () => {
-    let sum = 0;
-    for (let i = 0; i < revenue.length; i++) {
-      sum += revenue[i].value;
-    }
-    return sum;
-  };
+  const [totalRevenue, setTotalRevenue] = useState<number | null>(null);
+  const [totalExpense, setTotalExpense] = useState<number | null>(null);
 
-  const expSum = () => {
-    let sum = 0;
-    for (let i = 0; i < expense.length; i++) {
-      sum += expense[i].value;
-    }
-    return sum;
-  };
+  useEffect(() => {
+    const initialFetch = async () => {
+      try {
+        const [revenueResponse, expenseResponse] = await Promise.all([
+          fetch("http://localhost:3000/api/transactions/summary/revenue"),
+          fetch("http://localhost:3000/api/transactions/summary/expense"),
+        ]);
+
+        const [revenueData, expenseData] = await Promise.all([
+          revenueResponse.json(),
+          expenseResponse.json(),
+        ]);
+
+        setTotalRevenue(revenueData.totalRevenue);
+        setTotalExpense(expenseData.totalExpense);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    initialFetch();
+  }, []);
 
   return (
     <>
@@ -34,8 +40,13 @@ function SummarySection() {
               <AvatarFallback>Avatar</AvatarFallback>
             </Avatar>
             <div className="flex flex-col text-xl justify-center gap-5 lato">
-              <h1 className="font-bold">John Doe</h1>
-              <h2>{summation()} THB</h2>
+              <h1 className="font-bold">Wisarut</h1>
+              <h2>
+                {totalExpense !== null && totalRevenue !== null
+                  ? `${totalRevenue - totalExpense}`
+                  : "Loading..."}{" "}
+                THB
+              </h2>
             </div>
           </div>
 
@@ -44,13 +55,17 @@ function SummarySection() {
               <CardHeader>
                 <CardTitle>Total Revenue</CardTitle>
               </CardHeader>
-              <CardContent>{revSum()} THB</CardContent>
+              <CardContent>
+                {totalRevenue !== null ? `${totalRevenue} THB` : "Loading..."}
+              </CardContent>
             </Card>
             <Card className="flex w-40 h-24 text-center justify-center">
               <CardHeader>
                 <CardTitle>Total Expense</CardTitle>
               </CardHeader>
-              <CardContent>{expSum()} THB</CardContent>
+              <CardContent>
+                {totalExpense !== null ? `${totalExpense} THB` : "Loading..."}
+              </CardContent>
             </Card>
           </div>
         </div>
