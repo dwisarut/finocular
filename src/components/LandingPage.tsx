6c9b0a1f566200fc4fd4c6f369de7fbc6b176ca6
d_wisarut <d.wisarut@hotmail.com>
2026-01-09 18:07:15 +0700

    feat: add preview in landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -11,24 +11,61 @@ const LandingPage = () => {
 
   return (
     <>
-      <div className="flex flex-col container">
-        <h1 className="text-[hsl(0,0%,90%)] text-6xl md:text-8xl m-8 ml-6 md:ml-0 pb-4 w-xs md:w-3xl h-auto alata">
+      <div className="flex flex-col container gap-12">
+        <h1 className="text-[hsl(0,0%,90%)] text-6xl md:text-8xl mt-8 ml-6 md:ml-0 pb-4 w-xs md:w-3xl h-auto alata">
           Financial visualizer to track your{" "}
           <span className="text-amber-300">transaction</span>
         </h1>
-        <p className="text-[hsl(0,0%,90%)] text-xl lato ml-6 md:ml-0 m-8 w-[24rem] md:w-xl">
+        <p className="text-[hsl(0,0%,90%)] text-xl lato ml-6 md:ml-0 w-full lg:w-xl">
           Record your transaction history with the OCR's assistance for ease of
           use, with visualization to analyze your cash flow and spending
           behaviour.
         </p>
         <Button
           variant="outline"
-          className="flex flex-row w-42 rounded-2xl lato text-base items-center gap-4 ml-6 md:ml-0 hover:cursor-pointer"
+          className="flex flex-row w-42 rounded-2xl lato text-base items-center ml-6 md:ml-0 hover:cursor-pointer"
           onClick={() => handleNavigate("/ledger")}
         >
           Start here
           <ArrowRight />
         </Button>
+
+        <div className="flex flex-col gap-64 mt-48">
+          <div className="flex flex-col gap-8 lg:gap-0 lg:flex-row justify-start lg:justify-between">
+            <div className="flex flex-col justify-start gap-16 w-130">
+              <h2 className="text-xl alata">Transaction records</h2>
+              <h3 className="text-5xl alata">
+                In-depth record with customized tags
+              </h3>
+              <p className="text-xl lato">
+                Tagging each transaction for chart visualization in order to
+                view your statistics
+              </p>
+            </div>
+            <img
+              className="border rounded-2xl w-full lg:w-140"
+              src="/ledger-preview.svg"
+              alt="Ledger preview"
+            />
+          </div>
+          <div className="flex flex-col gap-8 lg:gap-0 lg:flex-row justify-start lg:justify-between">
+            <div className="flex flex-col justify-start gap-16 w-130">
+              <h2 className="text-xl alata">Dashboard & Visualization</h2>
+              <h3 className="text-5xl alata">
+                Visualization to aid with analyzing spending habits
+              </h3>
+              <p className="text-xl lato">
+                Summarize and visualize transactions as a line chart, and
+                categorize the spending habit as a donut chart.
+              </p>
+            </div>
+            <img
+              className="border rounded-2xl w-full lg:w-140"
+              src="/dashboard-preview.svg"
+              alt="Dashboard preview"
+            />
+          </div>
+        </div>
       </div>
       <footer className="border-t border-border mt-24">
         <div className="flex container flex-col mt-8 mb-8">
